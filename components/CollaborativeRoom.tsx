a5056cc1b1cd2ae2542a722e6e37dff762fd8f9c
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-10 00:27:45 +0530

    Refactor CollaborativeRoom component to include Header, ActiveCollaborators, and Editor

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import ActiveCollaborators from "./ActiveCollaborators";
+
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
+  return (
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex items-center justify-center gap-2 w-fit">
+              <p className="document-title">Test</p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
